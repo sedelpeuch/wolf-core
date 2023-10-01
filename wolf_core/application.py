a829af958d995899e6400e77d9dfb951a7016ffd
Sébastien Delpeuch <s.delpeuch@catie.fr>
2023-10-01 20:36:56 +0200

    Remove health check and status handling from application.py
    This commit removes health check and status handling code from `application.py` as these operations are now handled separately for better modularity and separation of concerns. The removed functionalities are health check of the application, setting status of the application, and shutdown method. Also, the version of the wolf_core package has been updated in `pyproject.toml`. These changes streamline the implementation, reducing internal dependencies and making the main application quirk cleaner and leaner.

--- wolf_core/application.py
+++ wolf_core/application.py
@@ -62,8 +62,6 @@ class Application(ABC):
 
         self._apis: List[api.API] = api.API.instances
 
-        self.__health_check = {"status": Status.WAITING, "message": " ", "last_execution": Status.NEVER, "id": -1, }
-
         self.app_lock = threading.Lock()
         self.__debug = False
         self.__setup_logger()
@@ -116,66 +114,6 @@ class Application(ABC):
             a.logger = self.logger
 
     @property
-    def health_check(self):
-        """
-        This property returns the health check of the application.
-
-        :return: The health check of the application
-        :rtype: bool
-        :raises: None
-        """
-        return self.__health_check
-
-    @health_check.setter
-    def health_check(self, value):
-        """
-        This method sets the health check of the application.
-
-        :param value: A dictionary representing the health check information.
-        :return: None
-        """
-        if not isinstance(value, dict):
-            raise TypeError("The health check must be a dict.")
-        try:
-            self.__health_check["status"] = value["status"]
-        except KeyError:
-            pass
-        try:
-            self.__health_check["message"] = value["message"]
-        except KeyError:
-            pass
-        try:
-            self.__health_check["last_execution"] = value["last_execution"]
-        except KeyError:
-            pass
-        try:
-            self.__health_check["id"] = value["id"]
-        except KeyError:
-            pass
-
-    @property
-    def status(self) -> Status:
-        """
-        This property returns the status of the application.
-
-        :return: The status of the application.
-        :rtype: Status.
-        """
-        return self.__health_check["status"]
-
-    def set_status(self, value: Status):
-        """
-        This method sets the status of the application.
-
-        :param value: The status value to be set.
-        Must be of type Status.
-        :return: None
-        """
-        if not isinstance(value, Status):
-            raise TypeError("The status must be a Status.")
-        self.health_check = {"status": value}
-
-    @property
     def debug(self) -> bool:
         """
         This property returns the debug mode of the application.
@@ -209,29 +147,6 @@ class Application(ABC):
                 return api_object
         raise ValueError("The API with name {} does not exist.".format(name))
 
-    def run(self):
-        """
-        The runner calls this method.
-        It loads the APIs, sets the frequency and schedules the job.
-
-        This method must not be overridden.
-        :return: None
-        """
-        self.app_lock.acquire()
-        self.health_check = {"status": Status.RUNNING, "id": uuid.uuid4().int, "message": " ", }
-        state = Status.RUNNING
-        try:
-            state = self.job()
-        except Exception as e:
-            self.health_check = {"status": Status.ERROR, "message": str(e)}
-            state = Status.ERROR
-            self.logger.error("An error occurred while running the application: {} - {}".format(type(e).__name__, e))
-            if self.__debug:
-                raise e
-        finally:
-            self.health_check = {"last_execution": self.status, "status": state}
-        self.app_lock.release()
-
     @abstractmethod
     def job(self) -> Status:
         """
@@ -243,11 +158,3 @@ class Application(ABC):
         """
         pass
 
-    def shutdown(self):
-        """
-        The runner calls this method when the application is stopped.
-        It must stop all the work of the application.
-
-        :return: None
-        """
-        pass
